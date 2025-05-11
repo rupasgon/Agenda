d27573ac901ab6f9853098d8999576c83ac094ee
Ruben Pascual Gonzalez <rupasgon@ioc.cat>
2025-05-11 07:43:56 +0200

    Afegida funcionalitat per mostrar contactes

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 console.log("Agenda Web");
+
+const contactes = ["Jose", "Saida", "Ruben"];
+const llista = document.createElement('ul');
+contactes.forEach(nom => {
+  const li = document.createElement('li');
+  li.textContent = nom;
+  llista.appendChild(li);
+});
+document.body.appendChild(llista);
+
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
   const input = document.querySelector("#nom");
