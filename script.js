5c7aa675837212280ffec4d63af658440f50ce4a
Marta Garcia <martagarcia@ioc.cat>
2025-05-11 08:08:50 +0200

    Control d’errors per camps buits al formulari

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Agenda Web");
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  const input = document.querySelector("#nom");
+  if (input.value.trim() === "") {
+    alert("El camp no pot estar buit");
+    return;
+  }
+  // Lògica per afegir el contacte
+});
+
